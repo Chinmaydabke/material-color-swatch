0246ebdfbad8cefd2629b76012fd4b3cd9c98d77
Chinmay Dabke <chinmaydabke@chinmays-air.home>
2020-08-23 23:28:43 +0100

    Updates

---
+++ precache-manifest.386d0f0793c8612462c2bce747e3daff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cb7ade7548c2bbea23e2ce701aff73d",
+    "url": "/material-color-swatch/index.html"
+  },
+  {
+    "revision": "a74407029a565e7045d9",
+    "url": "/material-color-swatch/static/css/main.b7fd03a5.chunk.css"
+  },
+  {
+    "revision": "a4f007f8988d493ab8a0",
+    "url": "/material-color-swatch/static/js/2.1ca98da6.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/material-color-swatch/static/js/2.1ca98da6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a74407029a565e7045d9",
+    "url": "/material-color-swatch/static/js/main.b4abc1d0.chunk.js"
+  },
+  {
+    "revision": "599185e95469213fd314",
+    "url": "/material-color-swatch/static/js/runtime-main.96c60a6d.js"
+  }
+]);
